8c73e3fb14d95a1e53d554123637b8aff9cbea6e
felipeFerreiraffl <felipado2x0@gmail.com>
2025-10-04 16:05:15 -0300

    refactor: changed onchange to input event to throw the need to click outside input

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -21,21 +21,24 @@ const state = document.getElementById("state");
 const neighborhood = document.getElementById("neighborhood");
 
 // Altera o valor dos campos automaticamente
-cep.onchange = (el) => {
-  fetch(`https://viacep.com.br/ws/${el.target.value}/json/`)
-    .then((res) => res.json())
-    .then((data) => {
-      setTimeout(() => {
-        street.value = data.logradouro;
-        state.value = data.estado;
-        neighborhood.value = data.bairro;
-      }, 1000);
-    })
-    .catch((err) => {
-      alert(`Erro ao acessar dados do CEP: ${err.message}`);
+cep.addEventListener("input", (el) => {
+  let cepValue = cep.value;
+  if (cepValue.length === 8) {
+    fetch(`https://viacep.com.br/ws/${el.target.value}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          street.value = data.logradouro;
+          state.value = data.estado;
+          neighborhood.value = data.bairro;
+        }, 1000);
+      })
+      .catch((err) => {
+        alert(`Erro ao acessar dados do CEP: ${err.message}`);
 
-      street.value = "";
-      state.value = "";
-      neighborhood.value = "";
-    });
-};
+        street.value = "";
+        state.value = "";
+        neighborhood.value = "";
+      });
+  }
+});
